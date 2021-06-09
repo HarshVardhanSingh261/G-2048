adc4be83f1f3a6571ec6fc6e58735a459211ffe0
HarshVardhanSingh26 <you@example.com>
2021-06-09 22:29:48 +0530

    version-2.0

--- src/index.js
+++ src/index.js
@@ -12,7 +12,9 @@ function GiveRand(min, max) {
 class Main extends React.Component {
   constructor(props) {
     super(props);
-
+    this.xDown = null;                                                        
+    this.yDown = null;
+    this.firstTouch = null;
     this.state = {
       board: [
         [0, 0, 0, 0],
@@ -21,6 +23,8 @@ class Main extends React.Component {
         [0, 0, 0, 0],
       ],
       gmaeOver: false,
+      score: 0,
+      Highscore: 0,
     };
   }
 
@@ -37,7 +41,7 @@ class Main extends React.Component {
       y2 = GiveRand(0, 4);
     board[x1][y1] = 2 * GiveRand(1, 3);
     board[x2][y2] = 2 * GiveRand(1, 3);
-    this.setState({ board, gmaeOver: false });
+    this.setState({ board, gmaeOver: false, score:0 });
   }
 
   getBlankCoordinates(Board) {
@@ -88,11 +92,13 @@ class Main extends React.Component {
         }
       }
     }
+    var score=0;
     for (let i = 0; i < 4; i++) {
       for(let j=0;j<3;j++){
         if(b[j][i]===b[j+1][i])
         {
           b[j][i]*=2
+          score+=b[j][i];
           var k=j+1
           while(k<3){
             b[k][i]=b[k+1][i]
@@ -106,7 +112,7 @@ class Main extends React.Component {
     if(this.isChanged(a,b))
       {b = this.placeRand(b)}
       
-    return b;
+    return {b,score};
   }
 
   down(board) {
@@ -128,11 +134,13 @@ class Main extends React.Component {
         }
       }
     }
+    var score=0;
     for (let i = 0; i < 4; i++) {
       for(let j=3;j>0;j--){
         if(b[j][i]===b[j-1][i])
         {
           b[j][i]*=2
+          score+=b[j][i]
           var k=j-1
           while(k>0){
             b[k][i]=b[k-1][i]
@@ -146,7 +154,7 @@ class Main extends React.Component {
     if(this.isChanged(a,b))
       {b = this.placeRand(b)}
 
-   return b;
+   return {b,score};
   }
 
   right(board) {
@@ -168,11 +176,13 @@ class Main extends React.Component {
         }
       }
     }
+    var score=0;
     for (let i = 0; i < 4; i++) {
       for(let j=3;j>0;j--){
         if(b[i][j]===b[i][j-1])
         {
           b[i][j]*=2
+          score+=b[i][j];
           var k=j-1
           while(k>0){
             b[i][k]=b[i][k-1]
@@ -186,7 +196,7 @@ class Main extends React.Component {
     if(this.isChanged(a,b))
       {b = this.placeRand(b)}
 
-   return b;
+   return {b,score};
   }
 
   left(board) {
@@ -208,11 +218,13 @@ class Main extends React.Component {
         }
       }
     }
+    var score=0;
     for (let i = 0; i < 4; i++) {
       for(let j=0;j<3;j++){
         if(b[i][j]===b[i][j+1])
         {
           b[i][j]*=2
+          score+=b[i][j]
           var k=j+1
           while(k<3){
             b[i][k]=b[i][k+1]
@@ -226,15 +238,15 @@ class Main extends React.Component {
     if(this.isChanged(a,b))
       {b = this.placeRand(b)}
 
-   return b;
+   return {b,score};
   }
 
   isGameOver(board){
     let PossMoves=[
-      this.isChanged(board,this.up(board)),
-      this.isChanged(board,this.down(board)),
-      this.isChanged(board,this.right(board)),
-      this.isChanged(board,this.left(board))
+      this.isChanged(board,this.up(board).b),
+      this.isChanged(board,this.down(board).b),
+      this.isChanged(board,this.right(board).b),
+      this.isChanged(board,this.left(board).b)
 
     ]
 
@@ -242,19 +254,67 @@ class Main extends React.Component {
 
   }
 
-  componentWillMount() {
+  componentDidMount() {
     this.initBoard();  
+    var hs = window.localStorage.getItem('score')
+    var hsi = (hs)?JSON.parse(hs):0;
+    this.setState({Highscore:hsi})
     const body = document.querySelector('body');
     body.addEventListener('keydown', this.handleKeyDown.bind(this));
+    document.addEventListener('touchstart', this.handleTouchStart.bind(this),false);        
+    document.addEventListener('touchmove', this.handleTouchMove.bind(this),false);
   }
   
+
+  // getTouches(evt) {
+  //   return evt.touches ||             // browser API
+  //         evt.originalEvent.touches; // jQuery
+  // }                                                     
+
+  handleTouchStart(evt) {
+      //console.log(evt);
+      this.firstTouch = (evt.touches ||             // browser API
+      evt.originalEvent.touches)[0];                                      
+      this.xDown = this.firstTouch.clientX;                                      
+      this.yDown = this.firstTouch.clientY;                                      
+  };                                                
+
+  handleTouchMove(evt) {
+      if ( ! this.xDown || ! this.yDown ) {
+          return;
+      }
+
+      var xUp = evt.touches[0].clientX;                                    
+      var yUp = evt.touches[0].clientY;
+
+      var xDiff = this.xDown - xUp;
+      var yDiff = this.yDown - yUp;
+      // console.log(this.xDown,this.yDown)
+      // console.log(xDiff,yDiff);
+      if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+          if ( xDiff > 0 ) {
+            this.makeMove('left');
+          } else {
+            this.makeMove('right');
+          }                       
+      } else {
+          if ( yDiff > 0 ) {
+            this.makeMove('up');
+          } else { 
+            this.makeMove('down');
+          }                                                                 
+      }
+      /* reset values */
+      this.xDown = null;
+      this.yDown = null;                                             
+  }
   handleKeyDown(e) {
     const up = 38;
     const right = 39;
     const down = 40;
     const left = 37
     const n = 78;
-    console.log(e.keyCode)
+    // console.log(e.keyCode)
     if (e.keyCode === up) {
       this.makeMove('up');
     } else if (e.keyCode === right) {
@@ -274,37 +334,58 @@ class Main extends React.Component {
   makeMove(dir){
     if(!this.state.gmaeOver){
       if(dir==='up'){
-        const b = this.up(this.state.board);
+        const obj = this.up(this.state.board);
+        const b = obj.b;
+        const score = obj.score;
         if(this.isGameOver(b))
-          this.setState({board:b, gmaeOver:true});
-        else this.setState({board:b,gmaeOver:false});
+          this.setState({board:b, gmaeOver:true, score:(score+this.state.score), Highscore:Math.max(score+this.state.score,this.state.Highscore)});
+        else this.setState({board:b,gmaeOver:false, score:(score+this.state.score), Highscore:Math.max(score+this.state.score,this.state.Highscore)});
       }
       else if(dir==='down'){
-        const b = this.down(this.state.board);
+        const obj = this.down(this.state.board);
+        const b = obj.b;
+        const score = obj.score;
         if(this.isGameOver(b))
-          this.setState({board:b, gmaeOver:true});
-        else this.setState({board:b,gmaeOver:false});
+          this.setState({board:b, gmaeOver:true, score:(score+this.state.score), Highscore:Math.max(score+this.state.score,this.state.Highscore)});
+        else this.setState({board:b,gmaeOver:false, score:(score+this.state.score), Highscore:Math.max(score+this.state.score,this.state.Highscore)});
       }
       else if(dir==='right'){
-        const b = this.right(this.state.board);
+        const obj = this.right(this.state.board);
+        const b = obj.b;
+        const score = obj.score;
         if(this.isGameOver(b))
-          this.setState({board:b, gmaeOver:true});
-        else this.setState({board:b,gmaeOver:false});
+          this.setState({board:b, gmaeOver:true, score:(score+this.state.score), Highscore:Math.max(score+this.state.score,this.state.Highscore)});
+        else this.setState({board:b,gmaeOver:false, score:(score+this.state.score), Highscore:Math.max(score+this.state.score,this.state.Highscore)});
       }
       else if(dir==='left'){
-        const b = this.left(this.state.board);
+        const obj = this.left(this.state.board);
+        const b = obj.b;
+        const score = obj.score;
         if(this.isGameOver(b))
-          this.setState({board:b, gmaeOver:true});
-        else this.setState({board:b,gmaeOver:false});
+          this.setState({board:b, gmaeOver:true, score:(score+this.state.score), Highscore:Math.max(score+this.state.score,this.state.Highscore)});
+        else this.setState({board:b,gmaeOver:false, score:(score+this.state.score), Highscore:Math.max(score+this.state.score,this.state.Highscore)});
       }
     }
+    window.localStorage.setItem('score',this.state.Highscore)
+  }
+
+  componentWillUnmount(){
     
+      window.localStorage.setItem('score',this.state.Highscore)
+      const body = document.querySelector('body');
+      body.removeEventListener('keydown', this.handleKeyDown.bind(this));
+      document.removeEventListener('touchstart', this.handleTouchStart.bind(this));        
+      document.removeEventListener('touchmove', this.handleTouchMove.bind(this));
+    
+    
+
   }
 
   render() {
     return (
       <div>
         <h1>2048!</h1>
+        <h3><span className="score">Score: {this.state.score}</span><span className="highscore"> High score: {this.state.Highscore}</span></h3>
         <table>
           {this.state.board.map((row, i) => (
             <Row key={i} row={row} />
